07561b3980d3ae8a77a4c19c113c7960bed143fd
Swapnil S. Mahajan <swapnilsm@gmail.com>
2014-01-20 04:22:32 +0530

    Added coding template

---
+++ t.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+#include <string>
+#include <cctype>
+#include <stack>
+#include <queue>
+#include <list>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <cmath>
+#include <bitset>
+#include <utility>
+#include <set>
+#include <numeric>
+#include <ctime>
+#define INF 2147483647
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+
+  cin.sync_with_stdio(false);
+  cout.sync_with_stdio(false);
+  int cases;
+  cin >> cases;
+  while(cases--) {
+
+  }
+  return 0;
+}
